fad4ecf926db49b340edb2186cef178287dd8064
katesheleh <kate.sheleh@gmail.com>
2020-05-02 13:55:06 +0200

    Updates

---
+++ precache-manifest.4c3494f744ff9cf920859800db4c53dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04f06d15d2f68719fac663cb8897f39",
+    "url": "/social-network-ts/index.html"
+  },
+  {
+    "revision": "aec42646c9591c3601d5",
+    "url": "/social-network-ts/static/css/main.58ac69f4.chunk.css"
+  },
+  {
+    "revision": "2c08a9c3d87c741c6d6d",
+    "url": "/social-network-ts/static/js/2.b99e396e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-network-ts/static/js/2.b99e396e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec42646c9591c3601d5",
+    "url": "/social-network-ts/static/js/main.3424b7b0.chunk.js"
+  },
+  {
+    "revision": "34663ad9dc236ca65477",
+    "url": "/social-network-ts/static/js/runtime-main.8af644e4.js"
+  }
+]);
