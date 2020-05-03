53b0830e836890537370d28b501d02ca93d5178b
katesheleh <kate.sheleh@gmail.com>
2020-05-03 14:06:58 +0200

    16-17 Beautify indents for Post Component

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -13,10 +13,9 @@ const Post = (props: PostType) => {
         <img src="https://placeimg.com/50/50/tech" alt="temporal" />
       </div>
       <div className={styles.content}>
-      <p className={styles.text}>{props.message}</p>
-      <div className={styles.like}>{props.likesCounter} likes</div>
+        <p className={styles.text}>{props.message}</p>
+        <div className={styles.like}>{props.likesCounter} likes</div>
       </div>
-  
     </div>
   )
 }
