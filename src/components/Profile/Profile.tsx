9b83803dafb8f2803adf597feb287f4f071163b0
katesheleh <kate.sheleh@gmail.com>
2020-06-03 21:19:38 +0200

    fix line spaces

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import styles from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
-import {PostsType} from '../../types/types';
+import { PostsType } from '../../types/types';
 
-const Profile=(props: PostsType) => {
+const Profile = ( props: PostsType ) => {
 
   return (
-    <div className={styles.profile}>
-      <h1>Profile</h1>
+    <div className={ styles.profile }>
+      <h1>Profile </h1>
       <ProfileInfo />
       <MyPosts
-        addPost={props.addPost}
-        posts={props.posts}
-        newPostText={props.newPostText}
-        updateNewPostText={props.updateNewPostText} />
+        addPost={ props.addPost }
+        posts={ props.posts }
+        newPostText={ props.newPostText }
+        updateNewPostText={ props.updateNewPostText } />
     </div>
-  )
-}
+  );
+};
 
 export default Profile;
